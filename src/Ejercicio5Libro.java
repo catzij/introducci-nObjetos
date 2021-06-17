861495d4a34e1e5a4bc991992531483ea93d35a9
Josué Casiá <josuecasia201831432@cunoc.edu.gt>
2021-06-16 20:12:26 -0600

    Creado Ejercicio5 y Ejercicio7 agregado a principal

---
+++ src/Ejercicio5Libro.java
@@ -0,0 +1,24 @@
+package src;
+
+public class Ejercicio5Libro{
+
+	public Ejercicio5Libro(){
+
+	}
+
+
+
+	public void prestamo(){
+
+	}
+
+	public void devolucion(){
+
+	}
+
+	public void mostrarIndormacion(){
+
+	}
+
+
+}
---
+++ src/Ejercicio7Fraccion.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Ejercicio7Fraccion{
+
+	public Ejercicio7Fraccion(){
+
+	}
+
+	public void sumar(){
+
+	}
+
+	public void multiplicar(){
+
+	}
+
+	public void dividir(){
+		
+	}
+}
